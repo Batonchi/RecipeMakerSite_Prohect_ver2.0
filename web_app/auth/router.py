7236a84f84c5478dccd5a70180d95d756e2d7e58
liShafor <elizavetashaforostovaxr@gmail.com>
2025-04-29 23:28:14 +0300

    Внесены изменения: - Доведена до ума авторизация и регистрация. (не конец)

--- web_app/auth/router.py
+++ web_app/auth/router.py
@@ -1,65 +1,100 @@
-# import uuid
-# import os
-# import shutil
+import uuid
+import os
+import shutil
+import datetime
+import flask
+from flask import Flask, render_template, request, redirect, url_for, make_response
+from typing import Optional
+from starlette.exceptions import HTTPException
+from datetime import date
+from web_app.auth.forms import LoginForm, RegisterForm
+from web_app.auth.service import create_token, hash_password
+from web_app.users.service import UserService
+
+router = flask.Blueprint('auth', __name__,
+                         url_prefix='/auth',
+                         static_folder='..web_app/view/static',
+                         template_folder='..web_app/view/')
+
+
+@router.get('/login')
+async def login_page():
+    return render_template('login.html', form=LoginForm())
+
+
+@router.post('/login')
+async def login():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    user = await UserService.get_one_or_none(email=email)
+    if not user or user.password != hash_password(password):
+        return render_template('login.html', form=LoginForm(), message="Invalid credentials")
+    token = create_token(email, password)
+    response = make_response(redirect(url_for('main_page')))
+    response.set_cookie('auth_token', token, httponly=True)
+    return response
+
+
+@router.get('/logout')
+async def logout():
+    response = make_response(redirect(url_for('auth.login_page')))
+    response.delete_cookie('auth_token')
+    return response
+
+
+@router.get('/register')
+async def register_page():
+    return render_template('reg.html', form=RegisterForm())
+
+
+@router.post('/register')
+async def register():
+    form = RegisterForm()
+    if form.password.data != form.password_again.data:
+        return render_template('reg.html', form=form, message="Пароли не совпадают.")
+    email = form.email.data
+    existing_user = await UserService.get_one_or_none(email=email)
+    if existing_user:
+        return render_template('reg.html', form=form, message="Такой пользователь уже существует.")
+
+    await UserService.insert(
+        **{
+            "name": form.name.data,
+            "surname": form.surname.data,
+            'created_date': datetime.datetime.now(),
+            'last_visit_date': datetime.datetime.now(),
+            'email': form.email.data,
+            'date_of_birth': form.date_of_birth.data,
+            'password': hash_password(form.password.data)
+        }
+    )
+    return redirect(url_for('auth.login_page'))
+
+
+# @router.post('/registration')
+# async def registration(first_name: str, last_name: str, email: str, password: str, special_word: str):
+#     avatar_uuid = uuid.uuid4()
+#     path = os.path.join('app/view/static/images/avatars/', f'{avatar_uuid}.png')
+#     shutil.copy('app/view/static/images/profile/default.png', path)
+#     try:
+#         await UserService.insert(first_name=first_name, last_name=last_name, email=email,
+#                                  password=hash_password(password),
+#                                  special_word=special_word)
+#     except Exception as ex:
+#         print(ex)
+#         raise HTTPException(status_code=409)
 #
-# import flask
-# from flask import render_template, request, redirect, url_for
-# from typing import Optional
-# from starlette.exceptions import HTTPException
-# from datetime import date
-# from web_app.auth.service import create_token, hash_password
-# from web_app.users.service import UserService
 #
-# router = flask.Blueprint('auth', __name__,
-#                          url_prefix='/auth',
-#                          static_folder='..web_app/view/static',
-#                          template_folder='..web_app/view/')
+# @router.get('/registration')
+# async def register_page(request: Request):
+#     return templates.TemplateResponse('registration.html', {'request': request})
 #
 #
-# @router.get('/login')
-# async def login_page(req: request):
-#     return render_template('login.html', {'request': req})
+# @router.get('/login/token')
+# async def login_with_token(request: Request):
+#     return templates.TemplateResponse('login_with_token.html', {'request': request})
 #
 #
-# # @router.get('/logout')
-# # async def logout(request: Request):
-# #     pass
-# #
-# #
-# # @router.post('/login')
-# # async def login(response: Response, email: str, password: str):
-# #     user = await UserService.get_one_or_none(email=email, password=hash_password(password))
-# #     if not user:
-# #         raise HTTPException(status_code=409, detail="Пользователь не найден! Неверный логин или пароль!")
-# #     token = create_token(email, password)
-# #     response.set_cookie("token", token, httponly=True)
-# #     return user.user_id
-# #
-# #
-# # @router.post('/registration')
-# # async def registration(first_name: str, last_name: str, email: str, password: str, special_word: str):
-# #     avatar_uuid = uuid.uuid4()
-# #     path = os.path.join('app/view/static/images/avatars/', f'{avatar_uuid}.png')
-# #     shutil.copy('app/view/static/images/profile/default.png', path)
-# #     try:
-# #         await UserService.insert(first_name=first_name, last_name=last_name, email=email,
-# #                                  password=hash_password(password),
-# #                                  special_word=special_word)
-# #     except Exception as ex:
-# #         print(ex)
-# #         raise HTTPException(status_code=409)
-# #
-# #
-# # @router.get('/registration')
-# # async def register_page(request: Request):
-# #     return templates.TemplateResponse('registration.html', {'request': request})
-# #
-# #
-# # @router.get('/login/token')
-# # async def login_with_token(request: Request):
-# #     return templates.TemplateResponse('login_with_token.html', {'request': request})
-# #
-# #
-# # @router.get('/successfully')
-# # async def successful_page(request: Request):
-# #     return templates.TemplateResponse('add_special_word.html', {'request': request})
+# @router.get('/successfully')
+# async def successful_page(request: Request):
+#     return templates.TemplateResponse('add_special_word.html', {'request': request})
--- web_app/main.py
+++ web_app/main.py
@@ -1,4 +1,5 @@
 import datetime
+import requests
 from flask import Flask, render_template, request, redirect, url_for, make_response
 from sqlalchemy.future import select
 from base.constant import HOST, PORT, SECRET_KEY
@@ -8,16 +9,13 @@ from base.database import async_session_maker
 from web_app.auth.service import hash_password, create_token
 from web_app.auth.forms import LoginForm, RegisterForm
 from dotenv import load_dotenv
-# from web_app.auth.router import router as auth_router
 from web_app.auth.router import router as auth_router
 import os
 
 
 app = Flask(__name__, static_url_path='/web_app/view/static', template_folder='view')
 app.secret_key = SECRET_KEY
-# app.register_blueprint(auth_router)
-
-
+app.register_blueprint(auth_router)
 
 
 @app.route('/')
@@ -33,53 +31,55 @@ def main_page():
         return 'DONE'
     except Exception as e:
         return f"Error: {str(e)}", 500
-    return 'hi'
 
 
-@app.route('/login', methods=['GET', 'POST'])
-async def login():
-    form = LoginForm()
-    if request.method == 'POST' and form.validate_on_submit():
-        async with async_session_maker() as session:  # создание сессии
-            """Проверяем пароль и почту; создаем токен"""
-            result1 = await session.execute(select(User).where(User.email == form.email.data))
-            result2 = await session.execute(select(User).where(User.password == hash_password(form.password.data)))
-            user = result1.scalars().first()
-            if user and result2.scalars().first():
-                # Создаем новый токен и сохраняем его в базе данных
-                token = create_token(email=form.email.data, password=hash_password(form.password.data))
-                user_token = UserToken(user_id=user.id, token=token)
-                session.add(user_token)  # Добавляем токен в сессию
-                await session.commit()  # Сохраняем изменения
-                response = make_response(redirect(url_for('main_page')))
-                response.set_cookie('auth_token', token, httponly=True)
-                return response
-            else:
-                return render_template('login.html', form=form, message="Почта или пароль введены неверно.")
-    return render_template('login.html', form=form)
+# @app.route('/login')
+# def login_page():
+#     form = LoginForm()
+#     return render_template('login.html', form=form)
 
 
-@app.route('/register', methods=['GET', 'POST'])
-async def register():
-    form = RegisterForm()  # форма регистрации
-    if request.method == 'POST' and form.validate_on_submit():
-        if form.password.data != form.password_again.data:
-            return render_template('reg.html', form=form, message="Пароли не совпадают")
-        async with async_session_maker() as session:  # создание сессии
-            result = await session.execute(select(User).where(User.email == form.email.data))
-            if result.scalars().first():
-                return render_template('reg.html', form=form, message="Такой пользователь уже существует")
-        await UserService.insert(**{
-            "name": form.name.data,
-            "surname": form.surname.data,
-            'created_date': datetime.datetime.now(),
-            'last_visit_date': datetime.datetime.now(),
-            'email': form.email.data,
-            'date_of_birth': form.date_of_birth.data,
-            'password': hash_password(form.password.data)
-        })  # добавляем нового пользователя в таблицу, бд
-        return redirect('/web/login')
-    return render_template('reg.html', form=form)
+# @app.route('/login', methods=['GET', 'POST'])
+# async def login():
+#     form = LoginForm()
+#     if request.method == 'POST' and form.validate_on_submit():
+#         async with async_session_maker() as session:  # создание сессии
+#             """Проверяем пароль и почту; создаем токен"""
+#             result1 = await session.execute(select(User).where(User.email == form.email.data))
+#             result2 = await session.execute(select(User).where(User.password == hash_password(form.password.data)))
+#             user = result1.scalars().first()
+#             if user and result2.scalars().first():
+#                 # # Создаем новый токен и сохраняем его в базе данных
+#                 #
+#                 # response = make_response(redirect(url_for('main_page')))
+#                 # response.set_cookie('auth_token', token, httponly=True)
+#                 return response
+#             else:
+#                 return render_template('login.html', form=form, message="Почта или пароль введены неверно.")
+#     return render_template('login.html', form=form)
+
+
+# @app.route('/register', methods=['GET', 'POST'])
+# async def register():
+#     form = RegisterForm()  # форма регистрации
+#     if request.method == 'POST' and form.validate_on_submit():
+#         if form.password.data != form.password_again.data:
+#             return render_template('reg.html', form=form, message="Пароли не совпадают")
+#         async with async_session_maker() as session:  # создание сессии
+#             result = await session.execute(select(User).where(User.email == form.email.data))
+#             if result.scalars().first():
+#                 return render_template('reg.html', form=form, message="Такой пользователь уже существует")
+#         await UserService.insert(**{
+#             "name": form.name.data,
+#             "surname": form.surname.data,
+#             'created_date': datetime.datetime.now(),
+#             'last_visit_date': datetime.datetime.now(),
+#             'email': form.email.data,
+#             'date_of_birth': form.date_of_birth.data,
+#             'password': hash_password(form.password.data)
+#         })  # добавляем нового пользователя в таблицу, бд
+#         return redirect('/web/login')
+#     return render_template('reg.html', form=form)
 #
 #
 # @app.route('/contact')
